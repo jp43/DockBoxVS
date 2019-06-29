1160eb3fba39ad52ed65ba05e08f7e72e094b949
PRETO Jordane <jpreto@pxe.cbp.ens-lyon.fr>
2019-06-30 01:06:24 +0200

    added new scripts

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if not (sys.version_info[0] == 2 and sys.version_info[1] >= 6):
 
 setuptools.setup(name='dockbox-vs',
     version='1.1',
-    scripts=['bin/prepare_compounds', 'bin/prepare_targets', 'bin/prepare_sites', 'bin/prepare_vs'],
+    scripts=['bin/prepare_compounds', 'bin/prepare_targets', 'bin/prepare_sites', 'bin/prepare_vs', 'bin/check_vs', 'bin/extract_from_first_round'],
     install_requires=['dockbox'],
     description='Plug-in for DockBox package',
     long_description=open('README.rst').read(),
