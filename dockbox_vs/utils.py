97fe205d81a96c820770213a9f263bd387ee6981
jordane.preto <jordane.preto@p2chpd-login3.local>
2020-01-28 10:51:16 +0100

    optimized prepare_compounds and prepare_vs

--- dockbox_vs/utils.py
+++ dockbox_vs/utils.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+known_formats = ['.sdf', '.smi', '.mol2']
+
 def get_number_of_compounds(file_l):
     suffix, ext = os.path.splitext(file_l)
     if ext == '.mol2':
@@ -11,3 +13,18 @@ def get_number_of_compounds(file_l):
 
     nligs = int(nligs)
     return nligs
+
+def get_babel_command(file_l, index=None):
+
+    suffix, ext = os.path.splitext(file_l)
+    extout = '.mol2'
+    for fmt in known_formats:
+        if ext == fmt:
+            input_format_flag = '-i' + ext[1:]
+            output_format_flag = '-o' + extout[1:]
+            if fmt in ['.smi', '.sdf']:
+                other_flags = '--gen3D'
+            else:
+                other_flags = ''
+    return 'babel %s %s %s ligand.mol2 -f %s -l %s %s 2>/dev/null'%(input_format_flag, file_l, output_format_flag, index, index, other_flags)
+
