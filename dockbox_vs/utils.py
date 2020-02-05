167493eeaeb671a2029f74006463e886ed0ee72a
PRETO Jordane <jpreto@pxe.cbp.ens-lyon.fr>
2020-02-05 19:14:30 +0100

    simplified prepare_vs

--- dockbox_vs/utils.py
+++ dockbox_vs/utils.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+nfolders_per_layer = 100 # number of folders per VS layer
 known_formats = ['.sdf', '.smi', '.mol2']
 
 def get_number_of_compounds(file_l):
@@ -27,4 +28,19 @@ def get_babel_command(file_l, index=None):
             else:
                 other_flags = ''
     return 'babel %s %s %s ligand.mol2 -f %s -l %s %s 2>/dev/null'%(input_format_flag, file_l, output_format_flag, index, index, other_flags)
+
+
+def get_subdir_from_ligid(ligid, layer_idx=1, nligands=0):
+    """get subdirectory name"""
+    minbin = (int(ligid[3:])-1)/nfolders_per_layer**layer_idx
+    minbin = minbin*nfolders_per_layer**layer_idx + 1
+
+    maxbin = min(minbin+nfolders_per_layer**layer_idx - 1, nligands)
+
+    minbin_str = str(minbin)
+    maxbin_str = str(maxbin)
+
+    minbin_str = (len(ligid[3:])-len(minbin_str))*'0'+minbin_str
+    maxbin_str = (len(ligid[3:])-len(maxbin_str))*'0'+maxbin_str
+    return '/lig' + minbin_str + '-' + maxbin_str
 
